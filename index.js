374b95df3c3b64eb256c41f139c38b36d7152878
Owen Smith <owen@omsmith.ca>
2015-11-20 14:10:57 -0500

    hoist functions

--- index.js
+++ index.js
@@ -23,31 +23,37 @@ module.exports = etag;
 
 function etag(options) {
   return function etag(ctx, next) {
-    return next().then(() => {
-      // no body
-      var body = ctx.body;
-      if (!body || ctx.response.get('ETag')) return;
-
-      // type
-      var status = ctx.status / 100 | 0;
-
-      // 2xx
-      if (2 != status) return;
-
-      if (body instanceof Stream) {
-        if (!body.path) return;
-        return fs.stat(body.path).catch(noop);
-      } else if (('string' == typeof body) || Buffer.isBuffer(body)) {
-        return body;
-      } else {
-        return JSON.stringify(body);
-      }
-    }).then(entity => {
-      if (!entity) return;
-
-      ctx.response.etag = calculate(entity, options);
-    });
+	  return next()
+		  .then(() => etagUp(ctx))
+		  .then(entity => etagCalculate(ctx, entity, options));
   };
+}
+
+function etagUp(ctx, options) {
+  // no body
+  var body = ctx.body;
+  if (!body || ctx.response.get('ETag')) return;
+
+  // type
+  var status = ctx.status / 100 | 0;
+
+  // 2xx
+  if (2 != status) return;
+
+  if (body instanceof Stream) {
+	if (!body.path) return;
+	return fs.stat(body.path).catch(noop);
+  } else if (('string' == typeof body) || Buffer.isBuffer(body)) {
+	return body;
+  } else {
+	return JSON.stringify(body);
+  }
+}
+
+function etagCalculate(ctx, entity, options) {
+	if (!entity) return;
+
+	ctx.response.etag = calculate(entity, options);
 }
 
 function noop() {}
